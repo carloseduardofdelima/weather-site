08519aec5c6b7b22dfbbcf55fd6ff828ccd14e41
Dudu <carloseduardoff12@gmail.com>
2020-07-21 18:00:06 -0300

    forecast script completed

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function setMainCardWheater (data) {
-    console.log(data);
     let main_card_title = document.querySelector('#main-card .card-title');
+    let main_card_icon = document.querySelector('#main-card .wheater-icon');
     let main_card_temperature = document.querySelector('#main-card .temperature');
     let main_card_sub_info = document.querySelector('#main-card .sub-info');
 
     main_card_title.innerHTML = data.WeatherText;
+    main_card_icon.src = `svg/${data.WeatherIcon}.svg`;
     main_card_temperature.innerHTML = `${data.Temperature.Metric.Value} °C`;
     main_card_sub_info.innerHTML = `
         Umidade: ${data.IndoorRelativeHumidity}%<br>
@@ -14,7 +15,6 @@ function setMainCardWheater (data) {
 }
 
 function setWeeklyForecast (data) {
-    console.log(data);
     let forecast_day = document.querySelectorAll('.forecast-day');
     let forecast_icon = document.querySelectorAll('.forecast-icon');
     let forecast_text = document.querySelectorAll('.forecast-text');
@@ -22,9 +22,25 @@ function setWeeklyForecast (data) {
     
     data.forEach((forecast, index) => {
         forecast_text[index].innerHTML = forecast.Day.ShortPhrase;
+        forecast_day[index].innerHTML = forecast.Date.slice(8, 10);
+        forecast_icon[index].src = `svg/${forecast.Day.Icon}.svg`;
     })
 
 
+}
+
+function setDailyForecast (data) {
+    console.log(data);
+    let forecast_icon = document.querySelectorAll('.daily-forecast .wheater-icon');
+    let forecast_text = document.querySelectorAll('.daily-forecast .card-text');
+
+    forecast_icon[0].src = `svg/${data.Day.Icon}.svg`;
+    forecast_icon[1].src = `svg/${data.Night.Icon}.svg`;
+
+    forecast_text[0].innerHTML = data.Day.IconPhrase;
+    forecast_text[1].innerHTML = data.Night.IconPhrase;
+
+    
 }
 
 async function setDefaultWheater() {
@@ -38,6 +54,11 @@ async function setDefaultWheater() {
     await axios.get('http://dataservice.accuweather.com/forecasts/v1/daily/5day/110258?apikey=dxDbkA6EfN8f83TwD1pEMSHhjCY9jBZO&details=true&language=pt-br')
     .then(response => {
         setWeeklyForecast(response.data.DailyForecasts);
+    })
+
+    await axios.get('http://dataservice.accuweather.com/forecasts/v1/daily/1day/110258?apikey=dxDbkA6EfN8f83TwD1pEMSHhjCY9jBZO&details=true&language=pt-br')
+    .then(response => {
+        setDailyForecast(response.data.DailyForecasts[0]);
     })
 
 
